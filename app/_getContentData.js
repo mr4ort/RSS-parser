79e1c3c2857bff03cebec7512219f3204ffe8c66
Igor Datsenko <mr4ort@gmail.com>
2016-09-22 00:34:43 +0300

    update

---
+++ app/_getContentData.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export default function () {
+
+  var $ = require('../node_modules/jquery/dist/jquery.min');
+  console.log($('body'));
+  let promis = new Promise(function (res, rej) {
+
+  })
+
+}
--- app/news.js
+++ app/news.js
@@ -1,4 +1,5 @@
 "use strict";
 
-console.log('dasfasdf');
+import getData from './_getContentData';
 
+getData();
